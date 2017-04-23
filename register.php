907a93f5ec01e8302d5ffe4aa0709c7279177866
daelyn <daelynoliveira3@gmail.com>
2017-04-23 15:08:37 +0530

    Add files via upload

---
+++ register.php
@@ -0,0 +1,29 @@
+<?php
+
+
+$connect=mysqli_connect("localhost", "root", "", "csi")or die("couldn't find server");
+echo "connected";
+
+//require("connect.php");
+
+/*$fn=$_POST["fname"];
+$mn=$_POST["mname"];
+$ln=$_POST["lname"];
+$dob=$_POST["dob"];
+$gen=$_POST["gen"];
+$contact=$_POST["num"];
+$em=$_POST["em"];
+$city=$_POST["ct"];
+$dept=$_POST["dept"];
+$yr=$_POST["yr"];
+$id=$_POST["id"];
+$skl=$_POST["sls"];
+$amt=$_POST["amt"];
+
+$write=mysqli_query($connect,"INSERT INTO csiform(fname,mname,lname,dob,gen,num,em,ct,dept,yr,id,sls,amt) values('$fn','$mn','$ln','$dob','$gen','$contact','$em','$city','$dept','$yr','$id','$skl','$amt')")or die("error cannot write data into database");
+
+echo "<br>succesfully added into database";*/
+
+?>
+
+
